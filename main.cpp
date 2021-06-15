c4a2414060e2d0e121187caafe7d7edf78ec5ddb
“nekitonn” <nekitonn@gmail.com>
2021-06-15 09:50:13 +0300

    -

--- main.cpp
+++ main.cpp
@@ -7,4 +7,12 @@ int main() {
     string Input;
     cout << "Type a word: ";
     cin >> Input;
+    int count_Letter_e(string Input);
+    int count_e = 0;
+    int count_er = 0;
+    for (int i = 0; Input[i] != '\0'; i++)
+        if (Input[i] == 'a'){
+            if (Input[i + 1] != 'b')
+                count_er++;
+        }
 }
